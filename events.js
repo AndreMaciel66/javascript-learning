a36e9f0f51cc18cd3dddd876dc9fb151de755521
Andre Maciel <andremaciel@MacBook-Pro.local>
2020-05-03 15:59:16 -0300

    minor fix

--- events.js
+++ events.js
@@ -3,30 +3,44 @@ const todoList = document.querySelector('#todo-list');
 const items = todoList.children;
 const todoNumber = document.querySelector(".todo-nr b");
 const mainTitle = document.querySelector(".main-title");
+const nameInput = document.querySelector(".name-input");
 
-console.log(mainTitle);
+console.log(todoList);
 
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    // Create element
     const newItem = document.createElement('li');
+
+    // adding class
     newItem.classList.add('item');
-    newItem.innerText = `Item ${items.length+1}`;
+
+    // Append ellement
+    newItem.innerText = nameInput.value;
+
     todoList.appendChild(newItem);
     todoNumber.innerText = items.length;
+
+    // remove value
+    nameInput.value="";
+
+    // Adding event listener
     newItem.addEventListener("click", deleteItem);
+
 });
 
 function deleteItem(e){
     e.stopPropagation();
     e.target.remove();
 }
+
 todoList.addEventListener('click', function(){
     todoList.classList.toggle('fade');
 })
 
-
-// button.addEventListener("keydown", function(){
-//     if (event.keyCode===81) {
-//         mainTitle.classList.toggle('spectacular');
-//     }
-// })
+button.addEventListener("click", function(){
+    mainTitle.classList.toggle('spectacular');
+})
